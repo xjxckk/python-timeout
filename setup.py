bbccba914d8b20dbd094610645e29bb0fd40cfb5
xjxckk <17519346+xjxckk@users.noreply.github.com>
2022-02-15 13:21:37 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name = 'timeout',
+    packages = ['timeout'],
+    version = '1.0',
+    description = 'Random timeout between minimum and maximum values',
+    url = 'https://github.com/xjxckk/Timeout/',
+    download_url = 'https://github.com/xjxckk/Timeout/archive/pypi-0_1_3.tar.gz',
+)
