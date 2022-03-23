7b8fe54e1ed0150504b8638e70652ec3c0ddbd8f
xJxckk <17519346+xjxckk@users.noreply.github.com>
2022-03-23 12:45:52 +0000

    update

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name = 'python-timeout',
     packages = ['timeout'],
-    version='2',
+    version='3',
     description = 'Random timeout between minimum and maximum values',
     url = 'https://github.com/xjxckk/python-timeout/',
     download_url = 'https://github.com/xjxckk/python-timeout/archive/refs/tags/v1.tar.gz',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown'
-)
+    )
