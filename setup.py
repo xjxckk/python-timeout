227ea378a833ab46ce5b59fa63ec4ec8bdcbeec2
j <>
2024-07-23 17:24:57 +0100

    Updates

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'python-timeout',
     packages = ['timeout'],
     install_requires = ['python-dateutil', 'python-printr'],
-    version='5.9',
+    version='6.0',
     description = 'Random timeout between minimum and maximum values',
     url = 'https://github.com/xjxckk/python-timeout/',
     long_description=open('README.md').read(),
